ad012b654902c79757ddceeed6cb01dba22bf432
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-09-25 15:04:57 +0200

    noedit

--- src/force/Force.java
+++ src/force/Force.java
@@ -14,7 +14,9 @@ public class Force {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for (int i = 0; i < 10; i++) {
+            
+        }
     }
     
 }
