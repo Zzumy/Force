e4ac4c6249ef4fc94486121b31aa77f9008f8302
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-09-25 15:21:23 +0200

    kiiras szöveg helyett megis

--- src/force/Force.java
+++ src/force/Force.java
@@ -4,11 +4,7 @@ public class Force {
 
     public static void main(String[] args) {
         while (true) {
-<<<<<<< HEAD
             System.out.println("kiir");
-=======
-            String txt = "szöveg";
->>>>>>> 1996643e2b083d0c6326688f8a5157b06ffdf374
         }
     }
     
